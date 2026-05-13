709f8afe51a73d064cb60c5fc4c59b704f49e356
jova0002 <jova0002@stud.kea.dk>
2026-05-13 12:02:15 +0200

    first-fixes

---
+++ src/assets/images/login.js
@@ -0,0 +1,118 @@
+class Login extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({
+      mode: "open",
+    }); //apparently slots only work with the shadow dom?
+  }
+  connectedCallback() {
+    this.html = `<style>
+      /****** LOGIN MODAL ******/
+      #login-modal {
+          background:#fff5e9;
+          position:fixed;
+          width:100vw;
+          height:100vh;
+      }
+      .loginmodal-container {
+        padding: 30px;
+        max-width: 350px;
+        width: 100% !important;
+        background-color: #ffe4e5;
+        margin: 0 auto;
+        border-radius: 2px;
+        box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+        overflow: hidden;
+        font-family: Instrument Sans;
+         color: #401609;
+      }
+      .loginmodal-container h1 {
+        text-align: center;
+        font-size: 1.8em;
+        color: #401609;
+      }
+      .loginmodal-container input[type=submit] {
+        width: 100%;
+        display: block;
+        margin-bottom: 10px;
+        position: relative;
+      }
+      input[type=password] {
+        height: 44px;
+        font-size: 16px;
+        width: 100%;
+        margin-bottom: 10px;
+        -webkit-appearance: none;
+        background: #fff;
+        border: 1px solid #401609;
+        border-top: 1px solid #401609;
+        padding: 0 8px;
+        box-sizing: border-box;
+      }
+      input[type=password]:hover {
+        border: 1px solid #401609;
+        border-top: 1px solid #401609;
+        box-shadow: inset 0 1px 2px rgba(0,0,0,0.1);
+      }
+      .loginmodal {
+        text-align: center;
+        font-size: 14px;
+        font-weight: 700;
+        height: 36px;
+        padding: 0 8px;
+      }
+      .loginmodal-submit {
+        border: 0px;
+        color: #fff;
+        text-shadow: 0 1px rgba(0,0,0,0.1); 
+        background-color: #401609;
+        padding: 17px 0px;
+        font-size: 14px;
+      }
+      .loginmodal-submit:hover {
+        border: 0px;
+        text-shadow: 0 1px rgba(0,0,0,0.3);
+        background-color: #1f3db0;
+      }
+    </style>
+    <div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="loginmodal-container">
+          <h1>G I R L T A L K</h1><br>
+          <p>Welcome to our Semester 3 Exam. We have redesigned "GirlTalk's" website and hope you like it</p>
+          <p>The password is <code>girltalk</code></p>
+          <form>
+            <input type="password" name="pass" placeholder="Password">
+            <input type="submit" name="login" class="login loginmodal-submit" value="Login">
+          </form>
+        </div>
+      </div>
+    </div>`;
+    this.render();
+
+    this.shadowRoot.querySelector("form").addEventListener("submit", (e) => {
+      e.preventDefault();
+      if (this.shadowRoot.querySelector("input[name=pass]").value === "girltalk") {
+        document.querySelector("#totally-delete-me").remove();
+        localStorage.setItem("iform-totally-logged-in", true);
+      }
+    });
+  }
+  render() {
+    this.shadowRoot.innerHTML = this.html;
+  }
+}
+customElements.define("iform-login", Login);
+window.addEventListener("load", () => {
+  if (!localStorage.getItem("iform-totally-logged-in")) {
+    const div = document.createElement("div");
+    div.id = "totally-delete-me";
+    div.style.width = "100vw";
+    div.style.height = "100vh";
+    div.style.position = "fixed";
+    div.style.zIndex = "9999";
+
+    div.innerHTML = "<iform-login />";
+    document.body.prepend(div);
+  }
+});
